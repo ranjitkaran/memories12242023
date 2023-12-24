8e3920b594ed6f7fd12c054162f396300a2f8f5e
ranjit-fexle-training <84138338+ranjitkaran@users.noreply.github.com>
2023-12-24 15:57:26 +0530

    memories app

---
+++ server/README.md
@@ -0,0 +1,4 @@
+# memories-app-MERN-Stack
+
+This is a social media app, using MongoDB database, Express.js, Node.js, React.js
+App Link -> https://memoriesranjit.herokuapp.com/
---
+++ server/config/prod.js
@@ -0,0 +1,8 @@
+require("dotenv").config()
+
+module.exports={
+    MONGOURI:process.env.MOGOURI,
+    JWT_SECRET:process.env.JWT_SEC,
+    SENDGRID_API:process.env.SENDGRID_API,
+    EMAIL:process.env.EMAIL
+}
